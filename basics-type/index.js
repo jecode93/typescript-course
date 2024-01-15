803e0243421efe61d446a4bd02b10295d6972dcd
jecode93 <jeanemmanuelcadet@gmail.com>
2024-01-15 14:14:08 -0500

    Basics type in TypeScript.

---
+++ basics-type/index.js
@@ -0,0 +1,2 @@
+var age = 28;
+console.log(age);
---
+++ basics-type/index.ts
@@ -0,0 +1,15 @@
+let age: number = 28
+let firstname: string = 'Mario'
+let isFictional: boolean
+
+age = 30 
+firstname = 'John'
+isFictional = true
+
+let planet = 'Earth'
+let moons = 1
+let isLarge = false
+
+planet = 'Saturn';
+moons = 145
+isLarge = true
