62afb65852098e5336267fce91a7be38dd4656a1
jecode93 <jeanemmanuelcadet@gmail.com>
2024-01-15 22:35:00 -0500

    Type guards in TypeScript.

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,25 @@
 "use strict";
 //------------
-// Union types
+// Types guards
 //------------
-let someId;
-someId = 1;
-someId = 'Hello';
-let email = null;
-email = 'jhon@doe.dev';
-email = null;
-let anotherId;
-anotherId = 'Good';
-anotherId = 7;
+function swapIdType(id) {
+    if (typeof id === 'string') {
+        // can use string methods
+        return parseInt(id);
+    }
+    else {
+        // can use anything specific to numbers
+        return id.toString();
+    }
+}
+const idOne = swapIdType(1);
+const idTwo = swapIdType('7');
+console.log(idOne, idTwo);
+function logDetails(value) {
+    if (value.type === 'user') {
+        console.log(value.email, value.username);
+    }
+    else {
+        console.log(value.firstName, value.age);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,46 @@
 //------------
-// Union types
+// Types guards
 //------------
 
-let someId: number | string
-
-someId = 1
-someId = 'Hello'
-
-let email: string | null = null
-
-email = 'jhon@doe.dev'
-email = null
-
 type Id = number | string
-let anotherId: Id
 
-anotherId = 'Good'
-anotherId = 7
+function swapIdType(id: Id) {
+  if (typeof id === 'string') {
+    // can use string methods
+    return parseInt(id)
+  } else {
+    // can use anything specific to numbers
+    return id.toString()
+  }
+}
 
+const idOne = swapIdType(1)
+const idTwo = swapIdType('7')
+
+console.log(idOne, idTwo);
+
+//------------------
+// Tagged interfaces
+//------------------
+
+interface User {
+  type: 'user'
+  username: string
+  email: string
+  id: Id
+}
+
+interface Person {
+  type: 'person'
+  firstName: string
+  age: number
+  id: Id
+}
+
+function logDetails(value: User | Person): void {
+  if (value.type === 'user') {
+    console.log(value.email, value.username);
+  } else {
+    console.log(value.firstName, value.age)
+  }
+}
