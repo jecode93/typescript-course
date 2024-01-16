e5094271d9e335060a35bd83d48a649f12b09688
jecode93 <jeanemmanuelcadet@gmail.com>
2024-01-15 19:11:12 -0500

    Function in TypeScript.

--- dist/index.js
+++ dist/index.js
@@ -1,35 +1,25 @@
 "use strict";
-//------
-// Arrays
-//------
-let names = ['John', 'Doe', 'Nene', 'Mario'];
-let ages = [25, 26, 27];
-names.push('Mido');
-ages.push(28);
-//------------------------------------
-// Type inference with Arrays
-//------------------------------------
-let fruits = ['Apple', 'Mangos', 'Bananas'];
-let people = [10, 11, 12, 13];
-fruits.push('Peaches');
-people.push(14);
-//---------------
-// Object literals
-//---------------
-let user = {
-    id: 1,
-    firstName: 'Mario',
-    age: 25
+//----------
+// Functions
+//----------
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+const substractTwoNmbers = (a, b) => {
+    return a - b;
 };
-user.firstName = 'John';
-user.age = 27;
-user.id = 2;
-//------------------------------------
-// Type inference with object literals
-//------------------------------------
-let person = {
-    name: 'Manno',
-    score: 25
-};
-person.name = 'Johnny';
-person.score = 26;
+addTwoNumbers(5, 4);
+substractTwoNmbers(10, 20);
+function addAllNumbers(items) {
+    const total = items.reduce((a, c) => a + c, 0);
+    console.log(total);
+}
+addAllNumbers([5, 6, 7, 8, 9]);
+//----------------------
+// Return type inference
+//----------------------
+function formatGreating(name, greating) {
+    return `${greating}, ${name}.`;
+}
+const result = formatGreating('John', 'Hello');
+console.log(result);
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,32 @@
-//------
-// Arrays
-//------
+//----------
+// Functions
+//----------
 
-let names: string[] = ['John', 'Doe', 'Nene', 'Mario'];
-let ages: number[] = [25, 26, 27];
-
-names.push('Mido')
-ages.push(28)
-
-//------------------------------------
-// Type inference with Arrays
-//------------------------------------
-
-let fruits = ['Apple', 'Mangos', 'Bananas']
-let people = [10, 11, 12, 13]
-
-fruits.push('Peaches')
-people.push(14)
-
-//---------------
-// Object literals
-//---------------
-
-let user: { firstName: string, age: number, id: number } = {
-  id: 1,
-  firstName: 'Mario',
-  age: 25
+function addTwoNumbers(a: any, b: any): number {
+  return a + b;
 }
 
-user.firstName = 'John'
-user.age = 27
-user.id = 2
-
-//------------------------------------
-// Type inference with object literals
-//------------------------------------
-
-let person = {
-  name: 'Manno',
-  score: 25
+const substractTwoNmbers = (a: number, b: number): number => {
+  return a - b;
 }
 
-person.name = 'Johnny'
-person.score = 26
+addTwoNumbers(5, 4)
+substractTwoNmbers(10, 20)
+
+function addAllNumbers(items: number[]) {
+  const total = items.reduce((a, c) => a + c, 0)
+  console.log(total);
+}
+
+addAllNumbers([5, 6, 7, 8, 9])
+
+//----------------------
+// Return type inference
+//----------------------
+
+function formatGreating(name: string, greating: string) {
+  return `${greating}, ${name}.`;
+}
+
+const result = formatGreating('John', 'Hello');
+console.log(result);
